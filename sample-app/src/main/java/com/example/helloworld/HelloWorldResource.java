43b0f383cc8c5345462637f7e1dcb93678b144c0
Timo Salm <52704922+tsalm-pivotal@users.noreply.github.com>
2022-07-11 18:29:28 +0200

    Update HelloWorldResource.java

--- sample-app/src/main/java/com/example/helloworld/HelloWorldResource.java
+++ sample-app/src/main/java/com/example/helloworld/HelloWorldResource.java
@@ -9,6 +9,6 @@ public class HelloWorldResource {
 
     @GetMapping
     public ResponseEntity<String> fetchGreeting() {
-        return ResponseEntity.ok("Hello Red!");
+        return ResponseEntity.ok("Hello Blue!");
     }
 }
